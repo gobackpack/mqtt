1a245f1cc654928669232d1c13126f763486c70b
Semir Mahovkic <semirm.dev@gmail.com>
2022-03-04 15:38:21 +0100

    mqtt

--- example/main.go
+++ example/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gobackpack/mqtt"
 	"github.com/sirupsen/logrus"
 	"log"
+	"sync"
 )
 
 func main() {
@@ -15,12 +16,19 @@ func main() {
 		logrus.Fatal(err)
 	}
 
+	wg := sync.WaitGroup{}
+	wg.Add(100)
+
 	for i := 0; i < 100; i++ {
-		go func(i int) {
+		go func(i int, wg *sync.WaitGroup) {
+			defer wg.Done()
+			
 			msg := fmt.Sprintf("message %d", i)
 			if token := mqttConn.Publish("my/topic", []byte(msg)); token.Wait() && token.Error() != nil {
 				log.Print("mqtt publish error: ", token.Error())
 			}
-		}(i)
+		}(i, &wg)
 	}
+
+	wg.Wait()
 }
